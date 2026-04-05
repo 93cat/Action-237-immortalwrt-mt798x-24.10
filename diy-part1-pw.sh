2ae67f47a4fa58928ac342c488b9a6024a4d5ac2
93cat <59479062+93cat@users.noreply.github.com>
2026-04-05 23:32:38 +0800

    Update diy-part1-pw.sh

--- diy-part1-pw.sh
+++ diy-part1-pw.sh
@@ -17,6 +17,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '1i src-git cloudflarespeedtest https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git' feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-# sed -i '1i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 sed -i '1i src-git lucky https://github.com/sirpdboy/luci-app-lucky.git;main' feeds.conf.default
 sed -i '1i src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
